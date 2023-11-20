1beb45284f89a1fd54e02a5156f9711606857ea2
emmanuel <emmanuel@fulodev.com>
2023-11-20 15:16:20 +0000

    adding ability to create report based on char count

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ def main():
     path_to_file = "books/frankenstein.txt"
     text = get_book_text(path_to_file)
     num_of_words = get_num_of_words(text)
+    char_count_dict = get_char_count(text)
+    chars_sorted_list = get_report_sorted_list(char_count_dict)
+
+    print(f"** Report of {path_to_file} **")
     print(f"{num_of_words} words found in document")
-    get_char_count(text)
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        else:
+            print(f"The '{item['char']}' character was found {item['num']} times")
 
 
 def get_book_text(path):
@@ -25,5 +35,18 @@ def get_char_count(text):
             char_count[char] = 1
 
     return char_count
+
+def sort_on(d):
+    return d["num"]
+
+def get_report_sorted_list(num_chars_dict):
+    sorted_list = []
+
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    return sorted_list
 
 main()
