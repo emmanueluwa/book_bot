1d9463d5f86a7736c8c490627afcb0f2c4e7d8ea
emmanuel <emmanuel@fulodev.com>
2023-11-20 13:44:20 +0000

    adding ability to calculate num of each char

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text = get_book_text(path_to_file)
     num_of_words = get_num_of_words(text)
     print(f"{num_of_words} words found in document")
+    get_char_count(text)
 
 
 def get_book_text(path):
@@ -13,5 +14,16 @@ def get_num_of_words(text):
     words = text.split()
     return len(words)
 
+def get_char_count(text):
+    lower_case_text = text.lower()
+
+    char_count = {}
+    for char in lower_case_text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    return char_count
 
 main()
